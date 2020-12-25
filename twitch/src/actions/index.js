75a950b75007bad6c3da4bcfe519a7096588eed3
Manish674 <manishchoudhary4905@gmail.com>
2020-12-25 16:31:07 +0530

    Working on streamlist comopnent

--- twitch/src/actions/index.js
+++ twitch/src/actions/index.js
@@ -1,10 +1,18 @@
-import stream from '../apis/stream'
-import { SIGN_IN, SIGN_OUT} from './types'
+import stream from "../apis/stream";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
 
 export const signIn = (userId) => {
   return {
     type: SIGN_IN,
-    payload: userId
+    payload: userId,
   };
 };
 
@@ -14,6 +22,33 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  stream.post('/stream',formValues)
-} 
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState.auth2;
+  const response = await stream.post("/stream", { ...formValues, userId });
+
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const response = await stream.get("/stream");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await stream.get(`stream/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const response = await stream.put(`/stream/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  stream.delete(`/stream/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload: id });
+};
--- twitch/src/components/streams/StreamList.js
+++ twitch/src/components/streams/StreamList.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-  return  (
-    <div>
-      <h3>StreamList</h3> 
-    </div>
-  )
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  renderList() {
+    return this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="ui large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="ui container">
+          <h3>Streams</h3>
+          <div className="ui celled list">
+            {this.renderList()}
+          </div> 
+        </div>
+      </div>
+    );
+  }
 }
 
-export default StreamList; 
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.stream) };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
